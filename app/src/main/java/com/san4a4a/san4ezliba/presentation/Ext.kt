99e194829538bdbc72ee83855e4fd26478d3968e
San4ezzz <rahlinskiysasha@gmail.com>
2023-02-28 18:14:44 +0100

    Extension fun fix

--- app/src/main/java/com/san4a4a/san4ezliba/presentation/Ext.kt
+++ app/src/main/java/com/san4a4a/san4ezliba/presentation/Ext.kt
@@ -1,10 +1,11 @@
 package com.san4a4a.san4ezliba.presentation
 
+import com.san4a4a.san4ezliba.utils.Cipher.encrypt
 import java.net.URLEncoder
 
 fun String.getDataFromMap(map: Map<String, Any?>?) : String?{
     return try {
-        map?.get(this).toString()
+        map?.get(encrypt(this)).toString()
     } catch (e: Exception) {
         null
     }
