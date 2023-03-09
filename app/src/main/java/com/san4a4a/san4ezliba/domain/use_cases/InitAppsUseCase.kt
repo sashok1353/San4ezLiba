60a177da76e072b2bdc878170331a73b842eeeae
San4ezzz <rahlinskiysasha@gmail.com>
2023-03-09 09:38:28 +0100

    Fix InitAppsUseCase

--- app/src/main/java/com/san4a4a/san4ezliba/domain/use_cases/InitAppsUseCase.kt
+++ app/src/main/java/com/san4a4a/san4ezliba/domain/use_cases/InitAppsUseCase.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import com.appsflyer.AppsFlyerLib
 import com.san4a4a.san4ezliba.data.appsflyer.AppsFlyerConversionListener
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class InitAppsUseCase @Inject constructor(@ApplicationContext private val context: Context,
 private val conversionListener: AppsFlyerConversionListener) {
-    operator fun invoke(appsId: String) {
-        AppsFlyerLib.getInstance()
-            .init(appsId, conversionListener.conversionListener, context)
-            .start(context)
+    suspend operator fun invoke(appsId: String) {
+        withContext(Dispatchers.IO) {
+            AppsFlyerLib.getInstance()
+                .init(appsId, conversionListener.conversionListener, context)
+                .start(context)
+        }
     }
 }
